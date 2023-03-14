ffa467a51d9b5db68f3728f4a599df48a00748ad
rubenderigo <rm.derigo@gmail.com>
2023-03-13 23:56:25 -0300

    Add eslint config

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
-'use client'
+'use client';
 
-import { Button } from '@chakra-ui/react'
+import { Button } from '@chakra-ui/react';
 
-import styles from './page.module.css'
+import styles from './page.module.css';
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Button>Button</Button>
     </main>
-  )
+  );
 }
