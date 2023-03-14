a9798f4ebaf6fa12138aeb3c4b44d40ed26f52f5
Nicolás Machado da Silva <nmachado.camejo@gmail.com>
2023-03-13 23:21:56 -0300

    Add eslint, prettier, & husky

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
-"use client";
+'use client'
 
-import { Button } from "@chakra-ui/react";
+import { Button } from '@chakra-ui/react'
 
-import styles from "./page.module.css";
+import styles from './page.module.css'
+
+const variable: number = 'string'
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Button>Button</Button>
     </main>
-  );
+  )
 }
