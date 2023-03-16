ab048b1d6aff1842897f5e98d96a0c341265781d
rubenderigo <rm.derigo@gmail.com>
2023-03-15 23:42:23 -0300

    feat(login-ui): add desktop design

--- app/login/index.jsx
+++ app/login/index.jsx
@@ -2,10 +2,11 @@ import {
   Box,
   Flex,
   InputGroup,
-  InputLeftElement,
   Text,
   Input,
   Button,
+  InputRightAddon,
+  Checkbox,
 } from '@chakra-ui/react';
 import { EmailIcon, LockIcon } from '@chakra-ui/icons';
 import Image from 'next/image';
@@ -14,42 +15,69 @@ import LogoFundacion from '../../assets/logo-fundacion.png';
 
 export const Login = () => (
   <Flex background="#F0F0F0" height="100vh" justify="center">
-    <Flex maxWidth="87vw" justify="between" margin="3rem">
-      <Box>
+    <Flex maxWidth="95vw" alignItems="start" justifyContent="center">
+      <Box width="28%" marginTop="3rem">
         <Image src={LogoFundacion} alt="Logo FUBE" />
         <Text fontSize={32} fontWeight={600}>
           Plataforma de Seguimiento de Casos de Abuso Infantil.
         </Text>
         <Text fontSize={18} fontWeight={400} color="gray">
-          Plataforma de Seguimiento de Casos de Abuso Infantil.
+          Trabajando juntos por la seguridad y el bienestar de los niños.
         </Text>
       </Box>
-      <Flex height="100%">
+      <Flex
+        width="50%"
+        justifyContent="flex-end"
+        alignItems="center"
+        height="100%"
+      >
         <Flex
           background="white"
           borderRadius="md"
-          paddingX="6"
-          paddingY="5"
+          paddingX="4rem"
+          paddingY="6rem"
           direction="column"
-          gap="5"
-          width={500}
+          gap="6"
+          width={480}
+          justifyContent="center"
         >
-          <Text fontSize={24} fontWeight={500}>
-            Iniciar Sesión
-          </Text>
-          <InputGroup>
-            <InputLeftElement>
-              <EmailIcon />
-            </InputLeftElement>
-            <Input placeholder="Email" />
-          </InputGroup>
-          <InputGroup>
-            <InputLeftElement>
-              <LockIcon />
-            </InputLeftElement>
-            <Input placeholder="Password" />
-          </InputGroup>
-          <Button>Iniciar sesion</Button>
+          <Box>
+            <Text fontSize={24} fontWeight={500}>
+              Iniciar Sesión
+            </Text>
+            <Text fontSize={18} fontWeight={400} color="#808080">
+              Ingresa tus credenciales para acceder a tu cuenta.
+            </Text>
+          </Box>
+
+          <Flex direction="column" gap="2">
+            <Text fontSize={16} fontWeight={400}>
+              Correo electronico*
+            </Text>
+            <InputGroup>
+              <Input placeholder="Ingresa tu correo..." />
+              <InputRightAddon>
+                <EmailIcon />
+              </InputRightAddon>
+            </InputGroup>
+          </Flex>
+
+          <Flex direction="column" gap="2">
+            <Text fontSize={16} fontWeight={400}>
+              Contraseña*
+            </Text>
+            <InputGroup>
+              <Input placeholder="Ingresa tu contraseña..." type="password" />
+              <InputRightAddon>
+                <LockIcon />
+              </InputRightAddon>
+            </InputGroup>
+            <Checkbox size="sm">
+              <Text fontSize={12}>Guardar sesión</Text>
+            </Checkbox>
+          </Flex>
+
+          <Button colorScheme="blue" size="lg">Iniciar sesión</Button>
         </Flex>
       </Flex>
     </Flex>
